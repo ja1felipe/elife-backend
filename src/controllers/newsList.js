e7a648ae180fa05240dcfd73ba65290a0b659b7f
João Felipe <39709263+ja1felipe@users.noreply.github.com>
2019-08-14 23:50:15 -0300

    changes

--- src/controllers/newsList.js
+++ src/controllers/newsList.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 router.get('/list', async (req,res) => {
     const lista = await News.find()
-    return res.send({lista})
+    return res.send(lista)
 })
 
 module.exports = (app) => app.use('/controller', router)
--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.json(), function (req, res, next) {
+    res.header("Allow", "OPTIONS, GET, POST")
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+    res.header("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, X-Requested-With, Content-Type, Accept, Authorization")
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    next()
+
+})
 
 require('./controllers/newsRegister')(app)
 require('./controllers/newsList')(app)
