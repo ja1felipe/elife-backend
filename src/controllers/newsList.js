612e5cdce9b0ff90eab77a4bd22b3c3c34c840ac
João Felipe <39709263+ja1felipe@users.noreply.github.com>
2019-08-18 16:58:40 -0300

    Acho que tudo pronto ate aqui

--- src/controllers/newsList.js
+++ src/controllers/newsList.js
@@ -8,6 +8,11 @@ router.get('/list', authMiddleWare, async (req,res) => {
     return res.send(lista)
 })
 
+router.get('/list/:theme', async (req,res) => {
+    const item = await News.find({theme : req.params.theme})
+    return res.send(item)
+})
+
 router.get('/list/:id', async (req,res) => {
     const item = await News.findById(req.params.id)
     return res.send(item)
